d6dc2de5c69529deeca04408f4b8fca2a3d0e476
Necdet Arda Etiman <necdetarda123@gmail.com>
2023-02-24 00:23:36 +0300

    bir hata tashih edildi

--- layout components/HeadInfo.js
+++ layout components/HeadInfo.js
@@ -6,8 +6,8 @@ export default function HeadInfo() {
   return (
     <>
         <Head>
-            <title>Necdet's Api Container</title>
-            <meta name='page description' content='Personal Api Container Of Necdet Arda'/>
+            <title>News Website</title>
+            <meta name='page description' content='News Website'/>
             <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
         </Head>
     </>
